df46ad11f30ddcb8dda12017727a1199ccf793e7
Kim_SJ <107867776+k-dev178@users.noreply.github.com>
2025-12-28 17:36:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
@@ -12,10 +13,18 @@ function onLoginSubmit(event){
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
-    localStorage.setItem("username",username)
+    localStorage.setItem(USERNAME_KEY,username)
 
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null){
+    
+} else{
+
+}
